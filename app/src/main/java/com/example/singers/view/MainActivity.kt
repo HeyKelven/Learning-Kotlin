d81170f3766466ff08f32d1ef6179983a6e71b75
kelven <vkelven@hotmail.com>
2020-03-03 03:51:54 -0300

    First class

--- app/src/main/java/com/example/singers/view/MainActivity.kt
+++ app/src/main/java/com/example/singers/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.singers
+package com.example.singers.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.singers.R
 
 class MainActivity : AppCompatActivity() {
 
